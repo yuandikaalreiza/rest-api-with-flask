67111bc3681421708e6767284a5ba924b3fae9ab
yuandikaalreiza <yuandikaalfahreiza@gmail.com>
2023-09-21 18:13:39 +0700

    create item in store

--- app.py
+++ app.py
@@ -27,3 +27,16 @@ def create_store():
     }
     stores.append(new_store)
     return new_store, 201
+
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {
+                "name": request_data["name"],
+                "price": request_data["price"]
+            }
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message": "store not found"}, 404
