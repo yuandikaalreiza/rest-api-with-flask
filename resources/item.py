7365865054a8bf78612794a0f55eb16b32d2a8bc
yuandikaalreiza <yuandikaalfahreiza@gmail.com>
2023-10-01 19:30:25 +0700

    validation with marshmallow

--- resources/item.py
+++ resources/item.py
@@ -3,6 +3,7 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from db import items
+from schemas import ItemSchema, ItemUpdateSchema
 
 blp = Blueprint("Items", __name__, description="Operations on stores")
 
@@ -20,12 +21,10 @@ class Item(MethodView):
             return {"message": "Item deleted."}
         except KeyError:
             abort(404, message="Item not found")
-
-    def put(self, item_id):
-        item_data = request.get_json()
-        if "price" not in item_data or 'name' not in item_data:
-            abort(400, message="Bad request. Ensure 'price', and 'name' are included in the JSON payload.")
-
+    
+    @blp.arguments(ItemUpdateSchema)
+    def put(self, item_data, item_id):
+        
         try:
             item  = items[item_id]
             item |= item_data
@@ -38,18 +37,9 @@ class Item(MethodView):
 class ItemList(MethodView):
     def get(self):
         return {"items": list(items.values())}
-
-    def post(self):
-        item_data = request.get_json()
-        if (
-            "price" not in item_data
-            or "store_id" not in item_data
-            or "name" not in item_data
-        ):
-            abort(
-                400,
-                message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
-            )
+    
+    @blp.arguments(ItemSchema)
+    def post(self, item_data):
         for item in items.values():
             if (
                 item_data["name"] == item["name"]
