a995404e433f657bc81ad9d8960dc41a15631763
yuandikaalreiza <yuandikaalfahreiza@gmail.com>
2023-10-03 20:42:20 +0700

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -16,6 +16,7 @@ class PlainTagSchema(Schema):
 class ItemSchema(PlainItemSchema):
     store_id = fields.Int(required=True, load_only=True)
     store = fields.Nested(PlainStoreSchema(), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
 
 class ItemUpdateSchema(Schema):
     name = fields.Str()
@@ -28,4 +29,10 @@ class StoreSchema(PlainStoreSchema):
 
 class TagSchema(PlainTagSchema):
     store_id = fields.Int(load_only=True)
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
     store = fields.Nested(PlainStoreSchema(), dump_only=True)
+
+class TagAndItemSchema(Schema):
+    message = fields.Str()
+    item = fields.Nested(ItemSchema)
+    tag = fields.Nested(TagSchema)
